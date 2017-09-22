8deddd7277b688f661e217d481e1e95eb9967e05
luisromo <changoluis@hotmail.com>
2017-09-22 01:16:53 -0500

    All

---
+++ HelloWorld/HelloWorld/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloWorld
+//
+//  Created by Luis Romo on 22/09/17.
+//  Copyright © 2017 devf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HelloWorld/HelloWorld/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  HelloWorld
+//
+//  Created by Luis Romo on 22/09/17.
+//  Copyright © 2017 devf. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
